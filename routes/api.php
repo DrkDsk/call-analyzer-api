594daa711a94cf6729058d8ce941c54b2d8a6716
Alfredo Palacios <joseaph.1998@gmail.com>
2026-06-25 12:21:41 -0600

    feat: add file upload and processing for phone event records

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\PhoneEventsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get('/user', static function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('files/upload', [PhoneEventsController::class, 'readFile'])->name('files.upload');
